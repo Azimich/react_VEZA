48639cf8aae85ac9a009f236abdebeabd712db6a
Рыболов Алексей Алексеевич <rybolov@veza.moscow>
2022-10-12 10:01:43 +0300

    VZ_10

---
+++ store/hooks/useIsInViewPort.ts
@@ -0,0 +1,26 @@
+import { useEffect, useMemo, useState } from "react";
+
+const useIsInViewport = (ref: { current: Element }) => {
+  const [isIntersecting, setIsIntersecting] = useState(false);
+
+  const observer = useMemo(
+    () =>
+      typeof IntersectionObserver !== `undefined` &&
+      new IntersectionObserver(([entry]) =>
+        setIsIntersecting(entry.isIntersecting)
+      ),
+    []
+  );
+
+  useEffect(() => {
+    observer.observe(ref.current);
+
+    return () => {
+      observer.disconnect();
+    };
+  }, [ref, observer]);
+
+  return isIntersecting;
+};
+
+export { useIsInViewport };
