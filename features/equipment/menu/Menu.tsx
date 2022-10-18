50fc518ebdd1b87042165130e85c53e5effedf99
Рыболов Алексей Алексеевич <rybolov@veza.moscow>
2022-10-18 17:52:17 +0300

    vz_30_01

--- features/equipment/menu/Menu.tsx
+++ features/equipment/menu/Menu.tsx
@@ -1,22 +1,49 @@
 import Styles from "../Equipment.module.scss";
 import { CategoryIcon } from "../../../components/icons";
 import { MenuItem } from "./MenuItem";
-import { CatalogEquipmentData } from "../mockData";
-
+import { breadcrumbsData } from "./mockData";
 import { ICatalogEquipmentData } from "../Equipment";
-import { useState } from "react";
-
-import { getLevel } from "../../../utils/helpers";
+import { useEffect, useState } from "react";
+import { CatalogEquipmentData } from "../mockData";
+import { useRouter } from "next/router";
 
 const Menu = () => {
+  const router = useRouter();
+
   const [menu, setMenu] = useState<ICatalogEquipmentData[]>(
     CatalogEquipmentData.filter((e) => e.parent_id === 0)
   );
 
-  const handleOnClick = (data: number) => {
-    console.log("getNested", getLevel(CatalogEquipmentData, data), data);
-    setMenu(CatalogEquipmentData.filter((e) => data === e.parent_id));
+  useEffect(() => {
+    console.log(router.query.slug);
+    if (router.query.slug) {
+      setMenu(
+        CatalogEquipmentData.filter((e) => {
+          return e.alias === router.asPath;
+        })[0].childrenData
+      );
+    } else {
+      setMenu(CatalogEquipmentData.filter((e) => e.parent_id === 0));
+    }
+  }, [router.query.slug]);
+
+  const handleOnClick = (data: ICatalogEquipmentData[], level: number) => {
+    let resData: ICatalogEquipmentData[] = [];
+    for (let i = 0; i < level; i++) {
+      resData.push(
+        (breadcrumbsData[i] = {
+          title: breadcrumbsData[i].title,
+          id: breadcrumbsData[i].id,
+          back: true,
+        })
+      );
+    }
+
+    let bySortId = resData.slice(0);
+    bySortId.sort((a, b) => b.id - a.id);
+    setMenu(resData.concat(data));
   };
+
   return (
     <div className={Styles.equipment__container_menu}>
       <h2 className={Styles.equipment__container_menu_title}>
@@ -26,7 +53,11 @@ const Menu = () => {
       <ul>
         {menu.map((e) => {
           return (
-            <MenuItem key={e.id} {...e} onClick={(e) => handleOnClick(e)} />
+            <MenuItem
+              key={e.id}
+              {...e}
+              onClick={(e, level, id) => handleOnClick(e, level, id)}
+            />
           );
         })}
       </ul>
