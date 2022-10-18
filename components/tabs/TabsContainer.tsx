6de398795cf4b2120d429e9ecb1ece340f20d1ca
Azim <azimov.an@veza.ru>
2022-10-18 14:13:05 +0300

    VZ_31

--- components/tabs/TabsContainer.tsx
+++ components/tabs/TabsContainer.tsx
@@ -16,25 +16,27 @@ const TabsContainer: FC<ITabsProps> = ({ props }) => {
   };
 
   return (
-    <div className={Styles.tabs__container}>
-      <ul className={Styles.tabs__container__menu}>
-        {props.length > 0 &&
-          props.map((e: ITab) => {
-            return (
-              <TabsItem
-                {...e}
-                key={e.id}
-                onClick={() => handleOnClick(e.id)}
-                tabsActive={activeTab}
-              />
-            );
-          })}
-      </ul>
-      <div className={Styles.tabs__container__line}></div>
+    <>
+      <div className={Styles.tabs__container}>
+        <ul className={Styles.tabs__container__menu}>
+          {props.length > 0 &&
+            props.map((e: ITab) => {
+              return (
+                <TabsItem
+                  {...e}
+                  key={e.id}
+                  onClick={() => handleOnClick(e.id)}
+                  tabsActive={activeTab}
+                />
+              );
+            })}
+        </ul>
+        <div className={Styles.tabs__container__line}></div>
+      </div>
       <div className={Styles.tabs__container_context}>
         {React.createElement(components[`tab_${activeTab}`])}
       </div>
-    </div>
+    </>
   );
 };
 
