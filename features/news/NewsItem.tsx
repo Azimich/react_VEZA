72e79fc807e51bc6422e572ba7d1755b1b85995d
rybolov <rybolov@yandex.ru>
2022-10-25 13:15:55 +0300

    vz_55_1

---
+++ features/news/NewsItem.tsx
@@ -0,0 +1,8 @@
+import { FC } from "react";
+import { INewDataItem } from "./News";
+
+const NewsItem: FC<INewDataItem> = ({ title }) => {
+  console.log("title", { title });
+  return <div>{title}</div>;
+};
+export { NewsItem };
