73538b814481bdc566666ca5dace5dea619ae885
rybolov <rybolov@yandex.ru>
2022-10-25 11:11:38 +0300

    vz_55_1

---
+++ features/news/News.d.ts
@@ -0,0 +1,17 @@
+interface INewsData {
+  id: number;
+  title: string;
+  desc_small: string;
+  desc_full: string;
+  alias: string;
+  count_view: number;
+  image_category: string;
+  image_full_news: string;
+  images_list_news: INewsImg[];
+}
+
+interface INewsImg {
+  image: string;
+}
+
+export { INewsData };
