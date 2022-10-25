33233e3186941eaaca9337322227ead67808cd4e
Azim <azimov.an@veza.ru>
2022-10-25 13:20:38 +0300

    VZ_55

---
+++ features/news/NewsPage/NeswPageItem.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+// import NewsData from "../../news/mockData";
+import { FC } from "react";
+
+const NeswPageItem: FC = () => {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima autem vel
+      repudiandae vero distinctio nihil dolor assumenda aut repellendus saepe,
+      soluta ducimus hic, omnis facere commodi sequi? Voluptatum, consectetur
+      odio.
+    </div>
+  );
+};
+
+export { NeswPageItem };
--- features/news/index.ts
+++ features/news/index.ts
@@ -1,2 +1,3 @@
 export { NewsContainer } from "./NewsContainer";
 export { NewsPage } from "./NewsPage/NewsPage";
+export { NeswPageItem } from "./NewsPage/NeswPageItem";
