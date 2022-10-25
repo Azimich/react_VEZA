709b2c1f10237a6581abaf9011a5d904d0eb9efa
rybolov <rybolov@yandex.ru>
2022-10-25 11:39:47 +0300

    vz_55_1

--- features/news/NewsContainer.tsx
+++ features/news/NewsContainer.tsx
@@ -1,6 +1,14 @@
 import { Container } from "../../components/common/container";
+import { INewsData } from "./News";
+import { FC, useEffect, useState } from "react";
 
-const NewsContainer = () => {
+const NewsContainer: FC<INewsData[]> = (props) => {
+  const [newsWithDesc, setNewsWithDesc] = useState<INewsData[]>(props);
+
+  useEffect(() => {
+    console.log("data", newsWithDesc);
+  }, []);
+
   return <Container className={"wrapper"}>111</Container>;
 };
 
