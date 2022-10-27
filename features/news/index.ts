70bfd3be45ab9f6e782038c8436fd959fd5cf2b0
Azim <azimov.an@veza.ru>
2022-10-27 17:42:19 +0300

    VZ_54

--- features/news/index.ts
+++ features/news/index.ts
@@ -1,4 +1,4 @@
 export { NewsContainer as News } from "./NewsContainer";
 export { NewsPage } from "./NewsPage/NewsPage";
-export { NewsPageItem } from "./NewsPage/NeswPageFerst";
+export { NewsPageItem } from "./NewsPage/NewsPageFirst";
 export { NewsWithItem } from "./NewsWithItem";
