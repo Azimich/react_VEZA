b39ec61495a106a684afb5be6905dc2fb80e725d
rybolov <rybolov@yandex.ru>
2022-10-31 09:15:14 +0300

    VZ_64

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,6 @@ import "/styles/media.scss";
 
 import type { AppProps } from "next/app";
 import { wrapper } from "../store/store";
-import "react-image-gallery/styles/scss/image-gallery.scss";
 import { Layout } from "../components/common";
 
 const VezaApp = ({ Component, pageProps }: AppProps) => {
