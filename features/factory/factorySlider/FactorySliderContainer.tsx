c8b3a05b1bbc7a6f144a53a378eaf53ff9029f87
Alex Rybolov <rybaalex@yandex.ru>
2022-10-31 23:58:32 +0300

    VZ_64

--- features/factory/factorySlider/FactorySliderContainer.tsx
+++ features/factory/factorySlider/FactorySliderContainer.tsx
@@ -3,31 +3,27 @@ import { sliderFactoryData } from "../factorySlider/MockData";
 import { SliderContainer } from "../../../components/slider";
 import Styles from "./FactorySliderContainer.module.scss";
 import { ISliderData } from "./FactorySlider";
-import { IComponents } from "../../../components/tabs/Tabs";
-import { Commercial, Industrial, Social } from "../../about";
 
 const FactorySliderContainer: FC<ISliderData> = () => {
   const [activePage, setActivePage] = useState<string>("2005");
   const handleOnClick = (code: string) => {
     setActivePage(code);
   };
-  const components: IComponents = {
-    tab_commercial: Commercial,
-    tab_industrial: Industrial,
-    tab_social: Social,
-  };
 
+  console.log("ooo", activePage);
   return (
     <div className={Styles.factory}>
       <SliderContainer
         items={sliderFactoryData.items}
         theme={"factorySlider"}
+        autoplay={false}
+        loop={false}
         slidesPerView={6}
         spaceBetween={8}
         navigations={true}
         OnClick={(code) => handleOnClick(code)}
       />
-      <div></div>
+      <div>{activePage}</div>
     </div>
   );
 };
