2248795314013f7be292833345492307705352ac
Azim <azimov.an@veza.ru>
2022-11-09 12:59:47 +0300

    VZ_95_2

--- features/resources/tab_bim/BimContainer.tsx
+++ features/resources/tab_bim/BimContainer.tsx
@@ -1,21 +1,25 @@
+import { useRouter } from "next/router";
+import React from "react";
+
 import { Container } from "../../../components/common/container";
 import Styles from "./Bim.module.scss";
 import { Tabs } from "../../../components/tabs";
 import { tabsResourcesData } from "../../contacts/mockData";
-
-import { useRouter } from "next/router";
 import { ITab } from "../../../components/tabs/Tabs";
 import { resourcesPath } from "../../../utils/bootstrap";
 import { BimItem } from "./";
 import { bimData } from "../mockData";
 import { Button } from "components/button";
+import { Modal, useModal } from "components/modal";
+import { ModalBim } from "./bimModal/ModalBim";
 
 const BimContainer = () => {
   const router = useRouter();
-
   const handleOnClickTabs = (e: ITab) => {
     router.push(resourcesPath + e.url);
   };
+
+  const { isShow, toggle } = useModal();
 
   return (
     <Container className={"wrapper"}>
@@ -35,8 +39,19 @@ const BimContainer = () => {
           size={"large"}
           children={"Скачать модель"}
           type={"button"}
+          onClick={toggle}
         />
       </div>
+
+      <Modal
+        isShow={isShow}
+        hide={toggle}
+        modalContent={<ModalBim />}
+        theme={"modal"}
+        headerText={"Тип файла"}
+        bgModal={"black"}
+      />
+
       <div className={Styles.bim__items}>
         {bimData.map((item, i) => {
           return <BimItem key={i} {...item} />;
---
+++ features/resources/tab_bim/bimModal/MockData.ts
@@ -0,0 +1,54 @@
+import IModalBimData from "./ModalBim.d";
+
+const modalBiData: IModalBimData[] = [
+  {
+    id: 1,
+    title: "BIM модели",
+  },
+  {
+    id: 2,
+    title: "Руководство",
+  },
+  {
+    id: 3,
+    title: "Техническая часть",
+  },
+  {
+    id: 4,
+    title: "BIM модели",
+  },
+  {
+    id: 5,
+    title: "Руководство",
+  },
+  {
+    id: 6,
+    title: "Техническая часть",
+  },
+  {
+    id: 7,
+    title: "BIM модели",
+  },
+  {
+    id: 8,
+    title: "Руководство",
+  },
+  {
+    id: 9,
+    title: "Техническая часть",
+  },
+  {
+    id: 10,
+    title: "BIM модели",
+  },
+  {
+    id: 11,
+    title: "Руководство",
+  },
+  {
+    id: 12,
+    title: "Техническая часть",
+  },
+];
+
+export { modalBiData };
