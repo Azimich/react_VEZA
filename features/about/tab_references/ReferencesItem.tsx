217573fb5940ea2ebec686b982f2ec90e08deb66
Azim <azimov.an@veza.ru>
2022-11-10 10:03:26 +0300

    VZ_92_2

--- features/about/tab_references/ReferencesItem.tsx
+++ features/about/tab_references/ReferencesItem.tsx
@@ -4,7 +4,10 @@ import Styles from "./References.module.scss";
 
 const ReferencesItem: FC<IReferencesImages> = (props) => {
   return (
-    <div className={Styles.image_block} onClick={() => props.onClick(props)}>
+    <div
+      className={Styles.image_block_item}
+      onClick={() => props.onClick(props)}
+    >
       <div className={Styles.image_block}>
         <img src={props.image} alt={props.title} />
       </div>
