655227339b7c8bb88fb599c91f1a869bac461739
Azim <azimov.an@veza.ru>
2022-11-23 17:41:41 +0300

    VZ_107

--- features/contacts/tab_sales_office/ModalFormOffice.tsx
+++ features/contacts/tab_sales_office/ModalFormOffice.tsx
@@ -4,31 +4,36 @@ import Styles from "components/map/Map.module.scss";
 import { EmailIcon, MapIcon, PhoneFooterIcon } from "components/icons";
 import { Link } from "components/link";
 
-const ModalFormOffice: FC<IObjectItem> = (e) => {
+const ModalFormOffice: FC<IObjectItem> = ({
+  fio,
+  address,
+  service_phone,
+  email,
+}) => {
   return (
     <div>
-      <div className={Styles.modal_title}>{e.name}</div>
+      <div className={Styles.modal_title}>{service_phone}</div>
       <p className={Styles.factory_info_fio}>
-        Руководитель филиала:<p>{e.fio}</p>
+        Руководитель филиала:<p>{fio}</p>
       </p>
       <div className={Styles.factory_info}>
         <p className={Styles.factory_info_services}>
           <MapIcon />
-          <span>{e.address}</span>
+          <span>{address}</span>
         </p>
         <p className={Styles.factory_info_services}>
           <PhoneFooterIcon />
-          <Link url={"tel:" + e.service_phone}>
+          <Link url={"tel:" + service_phone}>
             <a>
-              <span>{e.service_phone}</span>
+              <span>{service_phone}</span>
             </a>
           </Link>
         </p>
         <p className={Styles.factory_info_services}>
           <EmailIcon />
-          <Link url={"mailto:" + e.service_phone}>
+          <Link url={"mailto:" + service_phone}>
             <a>
-              <span>{e.email}</span>
+              <span>{email}</span>
             </a>
           </Link>
         </p>
