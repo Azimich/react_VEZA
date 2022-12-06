34abfcdce3e97c1476f267249e1d1f74ac617994
Alex Rybolov <rybaalex@yandex.ru>
2022-12-06 09:44:36 +0300

    VZ_125

--- pages/contacts/index.tsx
+++ pages/contacts/index.tsx
@@ -1,5 +1,5 @@
 import { useRouter } from "next/router";
-import { contactsPath } from "../../utils/bootstrap";
+import { contactsPath } from "utils/bootstrap";
 
 function RedirectPage() {
   const router = useRouter();
