131b583dbe848649dd2f0945484d3865319af91b
rybaalex <rybolovalex@yandex.ru>
2022-12-19 17:23:25 +0300

    VZ_137

--- service/list/servers/newsList.ts
+++ service/list/servers/newsList.ts
@@ -3,7 +3,7 @@ import { headers } from "utils/bootstrap";
 
 const newsList = async (pageNumber = 1, pageSize = 6, header = headers) => {
   return await FetchService.postData(
-    process.env.NEXT_PUBLIC_APP_FETCH + "/api/v1/get_news",
+    process.env.NEXT_PUBLIC_APP_FETCH + "/api/v1/news/get_news",
     {
       pageNumber: pageNumber,
       pageSize: pageSize,
