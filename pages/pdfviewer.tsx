f7d7be59e04055a233e78db3b0b9272b1b796fee
rybaalex <rybolovalex@yandex.ru>
2023-01-31 16:50:38 +0300

    VZ_205

--- pages/pdfviewer.tsx
+++ pages/pdfviewer.tsx
@@ -1,9 +1,12 @@
 import { GetStaticProps } from "next";
 import { wrapper } from "store/store";
 import { PdfViewer } from "features/resources/tab_certificates/certificates/PdfViewer";
+import { fetchMenu } from "store/slice/MenuSlice";
+import { menuListServer } from "service/index";
 
 export const getStaticProps: GetStaticProps = wrapper.getStaticProps(
-  () => async () => {
+  (store) => async () => {
+    store.dispatch(fetchMenu({ menuState: await menuListServer() }));
     return {
       props: {},
       revalidate: 10,
