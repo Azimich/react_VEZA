14b1f7ccddb49c5021fce3da292c0a9f70a9249a
Azim <azimov.an@veza.ru>
2023-01-31 15:59:12 +0300

    VZ_209

--- features/home/about/AboutItem.tsx
+++ features/home/about/AboutItem.tsx
@@ -1,30 +1,22 @@
 import { FC } from "react";
 import Styles from "./About.module.scss";
+import { IImages } from "types/response";
 
-interface AboutItem {
-  LinkClassName: string;
+interface IAboutItem {
   title: string;
-  desc: string;
-  keyLink: number;
-  img: string;
-  alt: string;
-  ImgClassName: string;
+  description: string;
+  images: IImages;
 }
 
-const AboutItems: FC<AboutItem> = ({
-  alt = "",
-  img = "",
-  title = "",
-  desc = "",
-}) => {
+const AboutItems: FC<IAboutItem> = ({ title, description, images }) => {
   return (
     <div className={Styles.card_user_item}>
       <div className={Styles.card_user_item_img}>
-        <img src={img} alt={alt} />
+        <img src={images.pc} alt={title} />
       </div>
       <div className={Styles.card_user_item_details}>
         <h4 className={Styles.title}>{title}</h4>
-        <p className={Styles.info}>{desc}</p>
+        <p className={Styles.info}>{description}</p>
       </div>
     </div>
   );
