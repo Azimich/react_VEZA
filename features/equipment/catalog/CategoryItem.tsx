4d1dbaffda7c8319bb3b446a6a38908a18f8e72a
rybaalex <rybolovalex@yandex.ru>
2023-02-14 16:52:13 +0300

    VZ_251

--- features/equipment/catalog/CategoryItem.tsx
+++ features/equipment/catalog/CategoryItem.tsx
@@ -6,7 +6,7 @@ import Styles from "../Equipment.module.scss";
 const CategoryItem: FC<ICategoriesItem> = ({ images, alias }) => {
   return (
     <Link url={alias} classLink={Styles.equipment__container_catalog_product_a}>
-      <img src={images && images.pc} alt={"Photo"} />
+      <img src={images.wide ? images.wide : images.pc} alt={"Photo"} />
     </Link>
   );
 };
