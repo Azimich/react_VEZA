3a84c5e5963f16fc101cd68977414bcbd45b45c0
Azim <azimov.an@veza.ru>
2023-02-15 11:50:12 +0300

    VZ_237_6

--- components/slider/SlideItem.tsx
+++ components/slider/SlideItem.tsx
@@ -26,6 +26,7 @@ const SlideItem: FC<ISlideItem> = ({
   const [hasWindow, setHasWindow] = useState(false);
   const isWindow = typeof window !== "undefined";
 
+  console.log("url", url);
   const Img = () => {
     return (
       <div
