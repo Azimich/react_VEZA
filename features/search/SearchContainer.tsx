fc28ae5dc1db44f1448c99463a989e7b44c64aeb
rybaalex <rybolovalex@yandex.ru>
2023-02-17 09:56:12 +0300

    VZ_260

--- features/search/SearchContainer.tsx
+++ features/search/SearchContainer.tsx
@@ -1,11 +1,9 @@
-import React, { FC, useEffect } from "react";
-import { ISearch, ISearchData } from "features/search/Search";
+import React, { FC } from "react";
+import { ISearch } from "features/search/Search";
 import { Container } from "components/common/container";
-import { SearchItem } from "features/search/SearchItem";
-import { Pagination } from "components/pagination/Pagination";
+
 import Styles from "components/common/header/search/SearchModal.module.scss";
 import { Input } from "components/input/InputContainer";
-import { searchData } from "components/common/header/search/mockData";
 import { useRouter } from "next/router";
 
 const SearchContainer: FC<ISearch> = () => {
@@ -13,16 +11,16 @@ const SearchContainer: FC<ISearch> = () => {
   const [inputValue, setInputValue] = React.useState<string>(
     router.query.q as string,
   );
-  const [filteredData, setFilteredData] = React.useState<ISearchData[]>([]);
-  useEffect(() => {
-    inputValue
-      ? setFilteredData(
-          searchData.filter((e) =>
-            e.title.toLowerCase().includes(inputValue.toLowerCase()),
-          ),
-        )
-      : setFilteredData([]);
-  }, [inputValue]);
+  /*const [filteredData] = React.useState<[]>([]);*/
+  /*  useEffect(() => {
+        inputValue
+          ? setFilteredData(
+              searchData.filter((e) =>
+                e.title.toLowerCase().includes(inputValue.toLowerCase()),
+              ),
+            )
+          : setFilteredData([]);
+      }, [inputValue]);*/
   const handleOnChangeSearch = (event: any) => {
     setInputValue(event.target.value);
   };
@@ -39,12 +37,12 @@ const SearchContainer: FC<ISearch> = () => {
           className={Styles.input_field}
         />
       </div>
-      {inputValue.length !== 0 && filteredData.length > 0
-        ? filteredData.map((e) => {
-            return <SearchItem {...e} key={e.id} />;
-          })
-        : inputValue.length !== 0 && <h2>Ничего не найдено</h2>}
-      <Pagination currentPage={1} totalPageCount={100} pageSize={10} />
+      {/*            {inputValue.length !== 0 && filteredData.length > 0
+                ? filteredData.map((e) => {
+                    return <SearchItem {...e} key={e.id}/>;
+                })
+                : inputValue.length !== 0 && <h2>Ничего не найдено</h2>}
+            <Pagination currentPage={1} totalPageCount={100} pageSize={10}/>*/}
     </Container>
   );
 };
