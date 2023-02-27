cecbe393b21dc44fa3856fab0f80152beb7fac22
Azim <azimov.an@veza.ru>
2023-02-27 14:39:17 +0300

    VZ_288

--- components/slider/SlideItem.tsx
+++ components/slider/SlideItem.tsx
@@ -36,6 +36,7 @@ const SlideItem: FC<ISlideItem> = ({
   const { isShow, toggle } = useModal();
   const { isShow: isShowAdd, toggle: toggleAdd } = useModal();
   const auth = useAppSelector(getAuth);
+
   const Img = () => {
     return (
       <>
@@ -43,6 +44,22 @@ const SlideItem: FC<ISlideItem> = ({
           className={`${Styles[`${theme}_banner__slider_slide`]}`}
           key={Math.random() * 10}
         >
+          {block_description && (
+            <div className={Styles[`${theme}_banner__right_element`]}>
+              <h1 className={Styles[`${theme}_banner__right_element_title`]}>
+                {title}
+              </h1>
+              <p className={Styles[`${theme}_banner__right_element_text`]}>
+                {description && description}
+              </p>
+              <Button theme={themeButton} size={size} link={url} color={color}>
+                <span>
+                  <p>Подробнее</p>
+                  {color === "link" && <ArrowRightIcon />}
+                </span>
+              </Button>
+            </div>
+          )}
           <div className={Styles[`${theme}_banner__slider_img`]}>
             {hasWindow && isDesktop && (
               <img
@@ -63,29 +80,6 @@ const SlideItem: FC<ISlideItem> = ({
               />
             )}
           </div>
-          {block_description && (
-            <div className={Styles[`${theme}_banner__right_element`]}>
-              <h1 className={Styles[`${theme}_banner__right_element_title`]}>
-                {title}
-              </h1>
-              <p className={Styles[`${theme}_banner__right_element_text`]}>
-                {description && description}
-              </p>
-              <div className={Styles[`${theme}_banner__right_element_allign`]}>
-                <Button
-                  theme={themeButton}
-                  size={size}
-                  link={url}
-                  color={color}
-                >
-                  <span>
-                    <p>Подробнее</p>
-                    {color === "link" && <ArrowRightIcon />}
-                  </span>
-                </Button>
-              </div>
-            </div>
-          )}
         </div>
       </>
     );
